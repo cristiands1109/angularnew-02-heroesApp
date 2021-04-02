babe50e7fc5e984a3d7b5b17f90417887a2030b8
Christian Duarte <cristiands1109@gmail.com>
2021-04-02 13:15:17 -0400

    Fin de la Seccion 12 - Inicio Heroes App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// modulos
+import { AppRouttingModule } from './app-routing.module'; // routing module
+
+// pages o componentes
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRouttingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+import { AuthRoutingModule } from './auth-routing.module';
+
+//pages o componentes
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegistroComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule
+  ]
+})
+export class AuthModule { }
